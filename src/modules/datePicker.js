3620bb8a660ac617f4d1657f388d0ed63e5751e3
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-08-26 16:54:03 +0530

    outside click added

--- src/modules/datePicker.js
+++ src/modules/datePicker.js
@@ -3,88 +3,24 @@ import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { Calendar } from 'react-date-range';
 import * as locales from 'react-date-range/dist/locale';
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 import '../styles/datePicker.scss';
+import useOutsideClick from './useOutsideClick';
 
-export default function DatePicker(){
-  const nameMapper = {
-  ar: 'Arabic',
-  bg: 'Bulgarian',
-  ca: 'Catalan',
-  cs: 'Czech',
-  cy: 'Welsh',
-  da: 'Danish',
-  de: 'German',
-  el: 'Greek',
-  enGB: 'English (United Kingdom)',
-  enUS: 'English (United States)',
-  eo: 'Esperanto',
-  es: 'Spanish',
-  et: 'Estonian',
-  faIR: 'Persian',
-  fi: 'Finnish',
-  fil: 'Filipino',
-  fr: 'French',
-  hi: 'Hindi',
-  hr: 'Croatian',
-  hu: 'Hungarian',
-  hy: 'Armenian',
-  id: 'Indonesian',
-  is: 'Icelandic',
-  it: 'Italian',
-  ja: 'Japanese',
-  ka: 'Georgian',
-  ko: 'Korean',
-  lt: 'Lithuanian',
-  lv: 'Latvian',
-  mk: 'Macedonian',
-  nb: 'Norwegian Bokmål',
-  nl: 'Dutch',
-  pl: 'Polish',
-  pt: 'Portuguese',
-  ro: 'Romanian',
-  ru: 'Russian',
-  sk: 'Slovak',
-  sl: 'Slovenian',
-  sr: 'Serbian',
-  sv: 'Swedish',
-  th: 'Thai',
-  tr: 'Turkish',
-  uk: 'Ukrainian',
-  vi: 'Vietnamese',
-  zhCN: 'Chinese Simplified',
-  zhTW: 'Chinese Traditional'
-};
-
-const localeOptions = Object.keys(locales)
-  .map(key => ({
-    value: key,
-    label: `${key} - ${nameMapper[key] || ''}`
-  }))
-  .filter(item => nameMapper[item.value]);
-
-const [locale, setLocale] = React.useState('enUS');
+export default function DatePicker(props){
+const [locale] = React.useState('es');
 const [date, setDate] = useState(null);
+ const ref = useRef();
+  useOutsideClick(ref, () => {
+    props.setOpen(false);
+  });
 return(
     <>
-    <div className="container">
-    <div className="datePickerWrapper">
-  <select 
-    className="dateSelect"
-    onChange={e => setLocale(e.target.value)}
-    value={locale}
-  >
-    {localeOptions.map((option, i) => (
-      <option value={option.value} key={i}>
-        {option.label}
-      </option>
-    ))}
-  </select>
+    <div ref={ref} className="datePickerWrapper">
   <Calendar 
   className="calendar"
   onChange={item => setDate(item)}
   locale={locales[locale]} date={date} />
-</div>
 </div>
 </>
 );
--- src/modules/mainPage.js
+++ src/modules/mainPage.js
@@ -3,15 +3,16 @@ import DatePicker from './datePicker';
 import '../styles/main.scss';
 export default function MainPage(){
     const [isToggle, setToggle] = useState(false);
+    const openFunc = () => setToggle(!isToggle);
     return(
         <>
         <div className="wrapper">
             <input 
-            onClick={()=>setToggle(!isToggle)}
+            onClick={openFunc}
             placeholder="Click here"
             >
             </input>
-        {isToggle && <DatePicker/>}
+        {isToggle && <DatePicker setOpen={openFunc}/>}
         </div>
         </>
     );
